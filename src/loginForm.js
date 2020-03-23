5080bda914190da16337e5172ee344de8ee24975
HungSun LIM <majac6@gmail.com>
2020-03-23 15:47:59 +0900

    각 페이지의 스크립트 호출방식 변경
    페이지가 활성화되었을때 관련된 js 를 init 하도록 변경

--- src/loginForm.js
+++ src/loginForm.js
@@ -1,37 +1,43 @@
-const addressInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const loginBtn = document.getElementById('loginBtn');
+function onInitLoginPage() {
+  if (!document.getElementById('email')) return;
 
-const login = () => {
+  const addressInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const loginBtn = document.getElementById('loginBtn');
+
+  const login = () => {
     if (addressInput.value) {
-        if (passwordInput.value) {
-            alert(`ID : ${addressInput.value}\nPW : ${passwordInput.value}`);
-            addressInput.value = '';
-            passwordInput.value = '';
-        } else {
-            alert('Type your Password');
-            passwordInput.focus();
-        }
+      if (passwordInput.value) {
+        alert(`ID : ${addressInput.value}\nPW : ${passwordInput.value}`);
+        addressInput.value = '';
+        passwordInput.value = '';
+      } else {
+        alert('Type your Password');
+        passwordInput.focus();
+      }
     } else {
-        alert('Type your E-mail address');
-        addressInput.focus();
+      alert('Type your E-mail address');
+      addressInput.focus();
     }
-}
+  };
 
-addressInput.addEventListener('keypress', e => {
+  addressInput.addEventListener('keypress', e => {
     if (e.keyCode === 13) {
-        addressInput.value ? passwordInput.focus() : alert('Type your E-mail address');
-        e.preventDefault();
+      addressInput.value
+        ? passwordInput.focus()
+        : alert('Type your E-mail address');
+      e.preventDefault();
     }
-})
+  });
 
-passwordInput.addEventListener('keypress', e => {
+  passwordInput.addEventListener('keypress', e => {
     if (e.keyCode === 13) {
-        login();
-        e.preventDefault();
+      login();
+      e.preventDefault();
     }
-})
+  });
 
-loginBtn.addEventListener('click', e => {
+  loginBtn.addEventListener('click', e => {
     login();
-})
+  });
+}
--- src/todo.js
+++ src/todo.js
@@ -1,147 +1,149 @@
-
 //HTML 변수선언
 
-const toDoList = document.getElementById('toDoList');
-const writeBox = document.getElementById('writeBox');
-const writeButton = document.getElementById('writeButton');
-const selDelbutton = document.getElementById('selDelbutton');
-const delAllbutton = document.getElementById('delAllbutton');
+function onInitTodoPage() {
+  if (!document.getElementById('toDoList')) return;
 
-var tdlArray = []; // 할일 오브젝트 배열
-let idNumber = 0;
+  const toDoList = document.getElementById('toDoList');
+  const writeBox = document.getElementById('writeBox');
+  const writeButton = document.getElementById('writeButton');
+  const selDelbutton = document.getElementById('selDelbutton');
+  const delAllbutton = document.getElementById('delAllbutton');
 
-//HTML 변수선언 끝!
+  var tdlArray = []; // 할일 오브젝트 배열
+  let idNumber = 0;
 
-//기능 HTHL
+  //HTML 변수선언 끝!
 
-const createLine = (targetElement) => {
+  //기능 HTHL
+
+  const createLine = targetElement => {
     var li = document.createElement('li');
     li.setAttribute('id', targetElement.doingID);
     li.setAttribute('class', 'list-group-item');
     li.setAttribute('draggable', 'true');
     li.innerHTML = `<input type="checkBox" class="checkBox" />
-        <span class="doingText">${targetElement.doing}</span>
-        <button class="editButton btn btn-info btn-sm">Edit</button>
-        <button class="delButton btn btn-danger btn-sm">\u00D7</button>`;
+          <span class="doingText">${targetElement.doing}</span>
+          <button class="editButton btn btn-info btn-sm">Edit</button>
+          <button class="delButton btn btn-danger btn-sm">\u00D7</button>`;
     toDoList.appendChild(li);
-}
+  };
 
-//기능 HTHL 끝!
+  //기능 HTHL 끝!
 
-load_tdl_array();
+  load_tdl_array();
 
-//엔터키
-writeBox.addEventListener('keydown', enter);
-function enter() {
+  //엔터키
+  writeBox.addEventListener('keydown', enter);
+  function enter() {
     var keyInput = window.event.keyCode;
     if (keyInput === 13) {
-        add_td();
+      add_td();
     }
-}
+  }
 
-//할일추가 이벤트 리스너
-writeButton.addEventListener('click', add_td);
+  //할일추가 이벤트 리스너
+  writeButton.addEventListener('click', add_td);
 
-//라인버튼 이벤트 리스너
-toDoList.addEventListener('click', button_td);
+  //라인버튼 이벤트 리스너
+  toDoList.addEventListener('click', button_td);
 
-//할일추가 리스트 업
-function add_td() {
+  //할일추가 리스트 업
+  function add_td() {
     if (!writeBox.value) {
-        alert('You have to write something.')
+      alert('You have to write something.');
     } else {
-        var Element = {
-            doingID: 'doingID_' + idNumber,
-            doing: writeBox.value,
-            finish: false,
-            updateTime: new Date()
-        }
-        tdlArray.push(Element);
-        createLine(Element);
+      var Element = {
+        doingID: 'doingID_' + idNumber,
+        doing: writeBox.value,
+        finish: false,
+        updateTime: new Date()
+      };
+      tdlArray.push(Element);
+      createLine(Element);
 
-        ++idNumber;
-        save_tdl_array();
+      ++idNumber;
+      save_tdl_array();
     }
     writeBox.value = '';
-}
+  }
 
-//라인버튼 스크립트
-function button_td(e) {
+  //라인버튼 스크립트
+  function button_td(e) {
     var targetTD = e.target.parentElement;
     var targetTDObject = tdlArray.find(Element => {
-        return Element.doingID === targetTD.getAttribute('id');
+      return Element.doingID === targetTD.getAttribute('id');
     });
 
     var targetClass = e.target.className;
     if (targetClass.includes('delButton')) {
-        tdlArray.splice(tdlArray.indexOf(targetTDObject), 1);
-        targetTD.parentElement.removeChild(targetTD);
-        save_tdl_array();
+      tdlArray.splice(tdlArray.indexOf(targetTDObject), 1);
+      targetTD.parentElement.removeChild(targetTD);
+      save_tdl_array();
     } else if (targetClass.includes('editButton')) {
-        var editInput = prompt('Write to edit', targetTDObject.doing);
-        if (editInput === null) {
-        } else if (!editInput) {
-            alert('You have to write something.');
-        } else if (editInput === targetTDObject.doing) {
-            alert('You have to write different.');
-        } else {
-            targetTDObject.doing = editInput;
-            targetTD.querySelector('.doingText').innerText = editInput;
-            save_tdl_array();
-        }
+      var editInput = prompt('Write to edit', targetTDObject.doing);
+      if (editInput === null) {
+      } else if (!editInput) {
+        alert('You have to write something.');
+      } else if (editInput === targetTDObject.doing) {
+        alert('You have to write different.');
+      } else {
+        targetTDObject.doing = editInput;
+        targetTD.querySelector('.doingText').innerText = editInput;
+        save_tdl_array();
+      }
     }
-}
+  }
 
-//선택항목 삭제
-selDelbutton.addEventListener('click', sel_del);
-function sel_del() {
+  //선택항목 삭제
+  selDelbutton.addEventListener('click', sel_del);
+  function sel_del() {
     var selLi = document.querySelectorAll('.checkBox:checked');
     if (selLi.length === 0) {
-        alert('You have to check to delete.')
+      alert('You have to check to delete.');
     } else {
-        var selDelCheck = confirm('Delete?');
-        if (selDelCheck === true) {
-            selLi.forEach(Element => {
-                var selLiParent = Element.parentElement;
-                var selTDObject = tdlArray.find(Element2 => {
-                    return Element2.doingID === selLiParent.getAttribute('id');
-                });
-                tdlArray.splice(tdlArray.indexOf(selTDObject), 1);
-                selLiParent.parentElement.removeChild(selLiParent);
-            })
-            save_tdl_array();
-        }
+      var selDelCheck = confirm('Delete?');
+      if (selDelCheck === true) {
+        selLi.forEach(Element => {
+          var selLiParent = Element.parentElement;
+          var selTDObject = tdlArray.find(Element2 => {
+            return Element2.doingID === selLiParent.getAttribute('id');
+          });
+          tdlArray.splice(tdlArray.indexOf(selTDObject), 1);
+          selLiParent.parentElement.removeChild(selLiParent);
+        });
+        save_tdl_array();
+      }
     }
-}
+  }
 
-//할일리스트 저장하기
-function save_tdl_array() {
+  //할일리스트 저장하기
+  function save_tdl_array() {
     window.localStorage.setItem('tdlArray', JSON.stringify(tdlArray));
     window.localStorage.setItem('idNumber', JSON.stringify(idNumber));
-}
+  }
 
-//할일리스트 가져오기
-function load_tdl_array() {
+  //할일리스트 가져오기
+  function load_tdl_array() {
     var lodedTdl = JSON.parse(window.localStorage.getItem('tdlArray'));
     var lodedIDNumber = JSON.parse(window.localStorage.getItem('idNumber'));
     if (typeof lodedIDNumber === 'number') {
-        idNumber = lodedIDNumber;
+      idNumber = lodedIDNumber;
     }
     if (Array.isArray(lodedTdl)) {
-        tdlArray = lodedTdl;
-        tdlArray.forEach(Element => {
-            createLine(Element);
-        })
+      tdlArray = lodedTdl;
+      tdlArray.forEach(Element => {
+        createLine(Element);
+      });
     } else if (!lodedTdl) {
-        tdlArray = [];
+      tdlArray = [];
     }
-}
+  }
 
-//초기화
-delAllbutton.addEventListener('click', clear_tdl_array)
-function clear_tdl_array() {
+  //초기화
+  delAllbutton.addEventListener('click', clear_tdl_array);
+  function clear_tdl_array() {
     window.localStorage.clear('tdlArray');
     window.localStorage.clear('idNumber');
     location.reload();
+  }
 }
-
