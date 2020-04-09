df67acdb70a56ae6d805a9ea02ae72c66acfc2ec
bekryoung <bekryoung@gmail.com>
2020-04-09 17:28:50 +0900

    -lesson 8 완료

---
+++ phone-book/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList  from './components/PhoneInfoList'
+
+
+class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
+  render() {
+    const {information} = this.state;
+    return (
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList
+          data={information}
+          onRemove={this.handleRemove}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
